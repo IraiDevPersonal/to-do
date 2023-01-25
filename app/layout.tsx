27bd33618f683ec2732bd9077531aea4ac7c0ab6
IraiDevPersonal <ignacio.arr01@gmail.com>
2023-01-24 22:24:10 -0300

    base projecto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,16 @@
+import { inter } from './fonts'
 import './globals.css'
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+function RootLayout({ children }: { children: React.ReactNode }) {
+   return (
+      <html lang="es" className={inter.variable}>
+         <head>
+            <title>To-do</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+         </head>
+         <body>{children}</body>
+      </html>
+   )
 }
+
+export default RootLayout
